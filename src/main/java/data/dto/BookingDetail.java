af917a6030db98f28aa94a39ca304eab9270ea2c
shvyeon <gustjsgp8569@naver.com>
2022-11-08 12:43:58 +0900

    dto setting2

--- src/main/java/data/dto/BookingDetail.java
+++ src/main/java/data/dto/BookingDetail.java
@@ -1,9 +1,13 @@
 package data.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.apache.ibatis.type.Alias;
 
 import java.sql.Timestamp;
 
+@Data
+@Alias("BookingDetail")
 public class BookingDetail {
     private int num;
     private String bookingTime;
--- src/main/java/data/dto/Precaution.java
+++ src/main/java/data/dto/Precaution.java
@@ -1,5 +1,10 @@
 package data.dto;
 
+import lombok.Data;
+import org.apache.ibatis.type.Alias;
+
+@Data
+@Alias("Precaution")
 public class Precaution {
     private int num;
     private String content;
