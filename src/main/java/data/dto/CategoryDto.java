e4c6f715aa7c655b9596c245b7ca4dbe5edd10f6
jhwon <anarose4815@naver.com>
2022-11-10 11:35:32 +0900

    mainpage category

--- src/main/java/data/dto/CategoryDto.java
+++ src/main/java/data/dto/CategoryDto.java
@@ -7,6 +7,8 @@ import org.apache.ibatis.type.Alias;
 @Alias("CategoryDto")
 public class CategoryDto {
     private int num;
-    private String name;
+    private String cname;
     private int mainCategoryNum;
+    
+    private String categoryImg; // 메인에 출력될 카테고리 이미지 컬럼 추가
 }
