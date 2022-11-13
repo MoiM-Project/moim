c34e7faec2d47dc76f37c73b141d87460a9494aa
shvyeon <gustjsgp8569@naver.com>
2022-11-14 01:14:05 +0900

    booking detail page - host info

--- src/main/java/data/controller/HostController.java
+++ src/main/java/data/controller/HostController.java
@@ -1,15 +1,32 @@
 package data.controller;
 
+import data.dto.HostDto;
+import data.mapper.HostMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 public class HostController {
 
+    @Autowired
+    HostMapper hostMapper;
+
     @GetMapping("/host")
     public String HostHome() {
         return "/home";
+    }
+
+    // booking detail page - host info
+    @GetMapping("/host/list")
+    public HostDto getHostInfoList(int num)
+    {
+        HostDto dto = hostMapper.getHostInfoList(num);
+//        System.out.println(dto);
+        return dto;
     }
 }
--- src/main/java/data/mapper/HostMapper.java
+++ src/main/java/data/mapper/HostMapper.java
@@ -1,10 +1,16 @@
 package data.mapper;
 
 
+import data.dto.HostDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 
 @Mapper
 public interface HostMapper {
     public void HostHome();
+
+    // booking detail page host info
+    public HostDto getHostInfoList(int num);
 }
