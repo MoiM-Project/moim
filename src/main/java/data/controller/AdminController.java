5001568c03bf8ef6e0d70c2158da303a234adf94
jhwon <anarose4815@naver.com>
2022-11-25 17:18:08 +0900

    adminNoticeUpdate

--- src/main/java/data/controller/AdminController.java
+++ src/main/java/data/controller/AdminController.java
@@ -27,7 +27,7 @@ public class AdminController {
 
     //파일 첨부를 위한 변수 선언
     String uploadFileName;
-    ArrayList<String> uploadFileNames = new ArrayList<>();
+
     @Autowired
     MemberMapper memberMapper;
 
@@ -167,13 +167,15 @@ public class AdminController {
     //관리자 페이지에서 공지사항 리스트 가져오기
     @GetMapping("/admin/noticeList")
     public List<NoticeDto> getNoticeSearchList(
-            @RequestParam String searchWord)
+            @RequestParam String searchWord, String sort)
     {
         //saerchWord 넘어오는지 테스트
         System.out.println("Notice searchWord = "+searchWord);
+        System.out.println("Notice sort = "+sort);
 
         HashMap<String, Object> map = new HashMap<>();
         map.put("searchWord",searchWord);
+        map.put("sort",sort);
 
         //map 출력 테스트
         System.out.println("NoticeList map = "+ map);
@@ -251,13 +253,27 @@ public class AdminController {
     //관리자 페이지에서 공지사항 삭제하기
     @DeleteMapping("/admin/deleteNotice")
     public void deleteNotice(
-            @RequestParam int num)
+            @RequestParam int num,
+            HttpServletRequest request)
     {
         //넘어온 방 번호 확인
         System.out.println("delete num값 확인 = "+num);
 
-        //방 번호 넘기기
+        //방 번호 넘겨서 정보 가져오기 (첨부 이미지 때문)
+        String oldPhoto = noticeMapper.getNoticeInfo(num).getImageUrl();
+
+        //파일 업로드 경로(path) 구하기
+        String path = request.getSession().getServletContext().getRealPath("/image");
+
+        //기존 업로드 파일이 있을 경우 path 경로에서 파일 삭제
+        if (oldPhoto != null) {
+            FileUtil.deletePhoto(path, oldPhoto);   //있을 경우 path 경로의 uploadFileName 을 지운다
+        }
+
+        //방 번호 넘겨서 notice DB 삭제하기
         noticeMapper.deleteNotice(num);
+        System.out.println("파일 삭제 완료");
+
     }
 
     //관리자 페이지에서 공지사항 리스트 가져오기 (modal에 띄우기)
@@ -275,11 +291,12 @@ public class AdminController {
 
     //관리자 페이지에서 공지사항 수정하기
     @PostMapping("/admin/updateNotice")
-    public void updateNotice (@RequestBody MultipartFile uploadFile,
+    public void updateNotice (@RequestBody MultipartFile updateFile,
                               HttpServletRequest request,
-                              @RequestParam String noticeType,
-                              String noticeTitle,
-                              String noticeContent,
+                              @RequestParam String updateType,
+                              String updateTitle,
+                              String updateContent,
+                              String oldPhoto,
                               int num
     ){
 
@@ -287,45 +304,47 @@ public class AdminController {
         HashMap<String, Object> map = new HashMap<>();
 
         //uploadFile을 제외하고 map에 담기
-        map.put("noticeType",noticeType);
-        map.put("noticeTitle",noticeTitle);
-        map.put("noticeContent",noticeContent);
+        map.put("updateType",updateType);
+        map.put("updateTitle",updateTitle);
+        map.put("updateContent",updateContent);
         map.put("num",num);
 
         //파일을 첨부했는지 안했는지 체크
         try {
 
             //upload 파일첨부를 했을때
-            if(uploadFile != null) {
+            if(updateFile != null) {
 
                 // 업로드할 폴더의 경로(path) 구하기
                 String path = request.getSession().getServletContext().getRealPath("/image");
 
                 //기존 업로드 파일이 있을 경우 path 경로에서 파일 삭제 후 다시 업로드
-                if (uploadFileName != null) {
-                    FileUtil.deletePhoto(path, uploadFileName);   //있을 경우 path 경로의 uploadFileName 을 지운다
+                if (oldPhoto != null) {
+                    FileUtil.deletePhoto(path, oldPhoto);   //있을 경우 path 경로의 oldPhoto 를 지운다
+                    System.out.println("기존 사진 oldPhoto 삭제 완료");
                 }
 
                 //업로드 파일을 변수에 담기
-                uploadFileName = uploadFile.getOriginalFilename();
+                uploadFileName = updateFile.getOriginalFilename();
 
-                //파일명을 날짜타입으로 변경
-                uploadFileName = ChangeName.getChangeFileName(uploadFile.getOriginalFilename());
+                //파일명을 날짜타입으로 변경(util 활용)
+                uploadFileName = ChangeName.getChangeFileName(updateFile.getOriginalFilename());
 
-                //path 경로에 파일 업로드 진행
-                uploadFile.transferTo(new File(path + "/" + uploadFileName));
+                //path 경로에 uploadFileName 의 파일명으로 업로드 진행
+                updateFile.transferTo(new File(path + "/" + uploadFileName));
 
                 //성공 시 콘솔에 찍기
-                System.out.println("파일 업로드 성공 -> 경로 // 파일명 " + path + "//" +uploadFileName );
+                System.out.println("신규 이미지 업로드 성공 -> 경로 // 파일명 " + path + "//" +uploadFileName );
 
-                //map 에 uploadFile 담기
-                map.put("uploadFile",uploadFileName);
+                //map 으로 updateFile에 파일명 담기
+                map.put("updateFile",uploadFileName);
 
                 //upload 파일첨부를 안했을때
             }else {
 
-                //map 에 uploadFile null 로 담기
-                map.put("uploadFile",null);
+                //map 으로 updateFile에 기존 사진(oldPhoto) 으로 담기
+                map.put("updateFile",oldPhoto);
+                System.out.println("기존 파일 유지!");
             }
 
         } catch (IllegalStateException e) {
@@ -338,7 +357,7 @@ public class AdminController {
         }
 
         // insert sql 에 map 전달
-        noticeMapper.noticeInsert(map);
+        noticeMapper.updateNotice(map);
     }
 
 
--- src/main/java/data/mapper/NoticeMapper.java
+++ src/main/java/data/mapper/NoticeMapper.java
@@ -9,6 +9,9 @@ import java.util.List;
 @Mapper
 public interface NoticeMapper {
 
+    List<NoticeDto> getNoticeList(HashMap<String, Object> map);
+
+    // 아래는 관리자용
     List<NoticeDto> getNoticeSearchList(HashMap<String, Object> map);
 
     void noticeInsert(HashMap<String, Object> map);
