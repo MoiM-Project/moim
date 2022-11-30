f222ec13607e58a4e724aefef2fd4bf2d4a45994
ksm980 <sung984@naver.com>
2022-11-30 17:56:33 +0900

    qna

--- src/main/java/data/mapper/QnAMapper.java
+++ src/main/java/data/mapper/QnAMapper.java
@@ -1,10 +1,14 @@
 package data.mapper;
 
+import data.dto.QnADto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.HashMap;
+import java.util.List;
 
 
 @Mapper
 public interface QnAMapper {
-
+    List<QnADto> getQnaByUserNum(HashMap<String,Object> map);
 
 }
