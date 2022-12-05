e85dad63d472e7614d1a92230c5035dd5d3cea36
chan <3233978@naver.com>
2022-12-05 15:22:20 +0900

    휴대폰 인증

---
+++ src/main/java/data/member/CertificationService.java
@@ -0,0 +1,13 @@
+package data.member;
+
+import com.amazonaws.services.s3.internal.eventstreaming.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+
+@Service
+public class CertificationService {
+}
